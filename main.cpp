a7f9f37be7e77f7354b0a6c772dcc80865688fea
Betobit <jesusmartinoza@gmail.com>
2015-12-06 15:25:09 -0600

    Nueva manera de hacer malla

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <graphics.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define NIMAGENES 2 // Numero de imagenes externas
+#define CUADRO_TAM 80 // Tamaño de cada cuadro en la malla.
 #define HEIGHT 700
-#define WIDTH 1024
+#define NIMAGENES 2 // Numero de imagenes externas.
+#define WIDTH 1040
 
 typedef char String[50];
 
+// Estructura principal del juego.
+typedef struct nodo
+{
+    int x1,y1;
+    int x2,y2;
+    int existe;
+    struct nodo *arriba;
+    struct nodo *abajo;
+    struct nodo *izq;
+    struct nodo *der;
+}Malla;
+
+int creaMalla(Malla *cab, int n, int m); // Crea la matriz donde iran los personajes y escenario.
+int creaNodo(Malla **cab, int x, int y); // Pide memoria para la creación de un nodo.
+int enlaza(Malla *m);
+int insertaHorizontal(Malla *cab, int x, int y);
+int insertaVertical(Malla *cab, int x, int y);
 void iniciaEntorno(); // Se encarga de iniciar la parte gráfica y obtener las imágenes del juego.
-void menu();
-void portada(); // Dibujar el menú principal
+void juego(); // Inicia juego
+void menu(); // Dibujar el menú principal
+void portada(); // Dibujar la pantalla principal
 
 // Variables globales.
 void *imagenes[NIMAGENES]; // Arreglo de imagenes en el juego
@@ -23,6 +43,62 @@ int main()
     getch();
     return 0;
 }
+
+int creaMalla(Malla *cab, int n, int m)
+{
+    int i, j, x=0, y=285, y2=y+CUADRO_TAM, res=0;
+    Malla *aux;
+    for(i=0; i<n; i++)
+    {
+        insertaHorizontal(cab, x+CUADRO_TAM*i, y);
+        for(aux=cab; aux; aux=aux->der);
+        for(j=0; j<m; j++)
+            insertaVertical(aux, x+CUADRO_TAM*i, y2+CUADRO_TAM*j);
+    }
+    res = enlaza(cab);
+    return res;
+}
+
+int creaNodo(Malla **cab, int x, int y)
+{
+    int res = 0;
+    *cab = (Malla*)malloc(sizeof(Malla));
+
+    if(*cab)
+    {
+        (*cab)->x1=x;
+        (*cab)->y1=y;
+        (*cab)->x2=x+CUADRO_TAM;
+        (*cab)->y2=y+CUADRO_TAM;
+        (*cab)->arriba=(*cab)->abajo=(*cab)->izq=(*cab)->der=NULL;
+        res=1;
+    }
+    return(res);
+}
+
+int enlaza(Malla *l)
+{
+    Malla *aux,*aux2,*aux3,*aux4;
+    int res=0;
+
+    if(l)
+        for(aux=l; aux; aux=aux->der)
+            for(aux2=aux; aux2; aux2=aux2->abajo)
+                if(aux2->abajo && aux->der)
+                {
+                    aux3 = aux->der->abajo;
+                    aux4 = aux->abajo;
+                    while(aux3 && aux4)
+                    {
+                        aux3->izq = aux4;
+                        aux4->der = aux3;
+                        aux3 = aux3->abajo;
+                        aux4 = aux4->abajo;
+                    }
+                }
+    return(res);
+}
+
 void iniciaEntorno()
 {
     initwindow(WIDTH, HEIGHT, "Run dino run");
@@ -42,9 +118,81 @@ void iniciaEntorno()
 
     // jungla
     bar(0,0,WIDTH,HEIGHT);
-    readimagefile(nombres[1],0,0,1024,300);
-    imagenes[1]  = malloc(imagesize(0,0,1024,300));
-    getimage(0,0,1024,300, imagenes[1]);
+    readimagefile(nombres[1],0,0,WIDTH,300);
+    imagenes[1]  = malloc(imagesize(0,0,WIDTH,300));
+    getimage(0,0,WIDTH,300, imagenes[1]);
+}
+
+int insertaHorizontal(Malla *cab, int x, int y)
+{
+    int res=0, band=1;
+    Malla *nuevo,*aux;
+
+    if(creaNodo(&nuevo, x, y))
+        if(!cab)
+         cab=nuevo;
+        else
+        {
+            for(aux=cab; aux!=NULL && band; aux=aux->der)
+                if(aux->der==NULL)
+                {
+                    aux->der=nuevo;
+                    nuevo->izq=aux;
+                }
+            setcolor(11);
+            rectangle(nuevo->x1,nuevo->y1,nuevo->x2,nuevo->y2);
+        }
+
+    return res;
+}
+
+int insertaVertical(Malla *l, int x, int y)
+{
+    int res=1;
+    Malla *nuevo, *aux;
+
+    if(creaNodo(&nuevo,x,y))
+    {
+        if(!l)
+        {
+            l=nuevo;
+            setcolor(5);
+            setfillstyle(1, rand()%15);
+            if(rand()%2)
+                bar((l)->x1, (l)->y1, (l)->x2, (l)->y2);
+        }
+        else
+        {
+            aux=l;
+            while(aux->abajo)
+                aux=aux->abajo;
+            aux->abajo=nuevo;
+            nuevo->arriba=aux;
+            setcolor(11);
+            rectangle(nuevo->x1,nuevo->y1,nuevo->x2,nuevo->y2);
+        }
+        res=1;
+    }
+    return(res);
+}
+
+void juego()
+{
+    // Borra todo y pone el fondo.
+    setfillstyle(1, COLOR(79, 182, 225));
+    bar(0,0, WIDTH, HEIGHT);
+    putimage(0, 400, imagenes[1], COPY_PUT);
+
+    // Inicia malla
+    Malla *cab = NULL;
+	creaMalla(cab, 13, 4);
+    /*for(i=0;i<226; i++)
+    {
+        //setfillstyle(1, COLOR(0,i,i));
+        setcolor(COLOR(79,182,i));
+        line(0,i,WIDTH,i);
+        delay(1);
+    }*/
 }
 
 void menu()
@@ -74,13 +222,14 @@ void menu()
                && ym<HEIGHT/2+margin+textheight(opciones[3])+10)
                 {
                     op = i;
+                    i=5;
                     break;
                 }
     }while(op==-1);
     switch(op)
     {
         case 0:
-            //juego(1, 0, 3);
+            juego();
             break;
         case 1:
             //imprimeRegistro();
@@ -101,7 +250,7 @@ void portada()
 
     for(i=0; i<50; i++)
     {
-        bar(0, 0, WIDTH, 400);
+        bar(0, 0, WIDTH,305);
         putimage(WIDTH/10, i, imagenes[0], COPY_PUT);
         delay(2);
     }
